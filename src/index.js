e0a6f0b206fc3039548c71189c1b43605176da60
hasibullahkhan <hasibpak@gmail.com>
2020-06-10 20:49:38 +0400

    updte index and app js files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import App from './App' ;
+
 ReactDOM.render(
- App(),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
