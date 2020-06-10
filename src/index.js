52de7f8b5f2df4d90f37ab3d1c8463840db6c475
hasibullahkhan <hasibpak@gmail.com>
2020-06-10 19:51:18 +0400

    update index.js
    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import app from './App' ;
+import App from './App' ;
 ReactDOM.render(
  
    App(),
