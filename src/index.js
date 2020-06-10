b3acc481f51dd1ac3807e6ade683e4b688f53366
hasibullahkhan <hasibpak@gmail.com>
2020-06-10 19:43:26 +0400

    update js
    update js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 import app from './App' ;
 ReactDOM.render(
-  <React.StrictMode>
-   <App />
-  </React.StrictMode>,
+ 
+   <App />,
   
   document.getElementById('root')
 );
